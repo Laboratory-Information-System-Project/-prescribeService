3d358bab092a6ca03fabd82223dae0c6e21a25fd
liverbird <ehdgns07@gmail.com>
2022-11-07 11:14:14 +0900

    init

---
+++ src/main/java/com/douzone/prescribeservice/PrescribeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.douzone.prescribeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescribeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrescribeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/douzone/prescribeservice/PrescribeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douzone.prescribeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrescribeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
